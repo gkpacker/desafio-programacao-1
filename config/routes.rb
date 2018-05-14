8836fd22bef74dcbba6115c25907c3e3f4a97b2a
Gabriel Packer <ggkpacker@gmail.com>
2018-05-14 19:56:45 -0300

    Generate controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :incomes, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
